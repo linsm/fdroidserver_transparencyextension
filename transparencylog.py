2600ce68ce256f44039b464f2addce4a43182f05
Mario Lins <mario.lins@ins.jku.at>
2023-02-02 10:50:01 +0100

    update script and prepare for submission

--- transparencylog.py
+++ transparencylog.py
@@ -1,11 +1,32 @@
 """
-Copyright (c) 2022 Mario Lins <mario.lins@ins.jku.at>
+Copyright (c) 2023 Mario Lins <mario.lins@ins.jku.at>
 
 Licensed under the EUPL, Version 1.2. 
   
 You may obtain a copy of the Licence at: 
 https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
 """
+#!/usr/bin/env python3
+
+import collections
+import defusedxml.minidom
+import git
+import glob
+import hashlib
+import os
+import json
+import logging
+import requests
+import shutil
+import tempfile
+import zipfile
+from argparse import ArgumentParser
+
+from . import _
+from . import common
+from . import deploy
+from .exception import FDroidException
+
 
 def main():
     global options
@@ -13,7 +34,7 @@ def main():
     parser = ArgumentParser()
     common.setup_global_opts(parser)
     parser.add_argument("--personalityaddress",
-                        default="http://IP_REST_API:PORT/",
+            default="https://ENTER_DEFAULT_IP:PORT/",
                         help=_("Path to the the REST API of the personality"))
     parser.add_argument("--appid",
                         default="com.company.appid",
@@ -22,7 +43,7 @@ def main():
                         default="1",
                         help=_("App Version"))
     parser.add_argument("--treeid",
-                        default="5253222656226122581",
+                        default="ENTER_DEFAULT_TREE_ID",
                         help=_("Tree ID"))
     options = parser.parse_args()
 
@@ -33,10 +54,19 @@ def main():
         for blk in iter(lambda: f.read(4096),b""):
             sha256_hash.update(blk)
     print(sha256_hash.hexdigest())
-    
+
     createlogurl = options.personalityaddress + 'Log/AddLogEntry?treeId=' +options.treeid
     logentry = {"applicationId":options.appid,"version":options.version,"signatureData":sha256_hash.hexdigest()}
-    response = requests.post(createlogurl,json=logentry)
+    
+    createloginurl = options.personalityaddress + 'Login/Login'
+    logincredentials = {"username": "buildserver", "password":"ENTER_PASSWORD"}
+    loginresponse = requests.post(createloginurl,json=logincredentials,verify='PATH_TO_CERTIFICATE')
+    #print(loginresponse.json())
+    token = loginresponse.json()['token']
+    print(token)
+
+    response = requests.post(createlogurl,headers={'Authorization': 'Bearer '+token},json=logentry,verify='PATH_TO_CERTIFICATE')
+    print(response)
     print(response.json())
     print(response.status_code)
     print(createlogurl)
